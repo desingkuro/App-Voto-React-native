360be266f5d6d82e27b4f697c193ea1418dd20fc
desingkuro <andresmauricio745@gmail.com>
2023-03-09 14:37:14 -0500

    se añadio el sistema de voto para cada postulado

--- components/Voto.js
+++ components/Voto.js
@@ -6,51 +6,76 @@ import { Contralor } from "../screens/Contralor";
 import { Personero } from "../screens/Personero";
 
 export const Voto = (props) => {
-const { selectedId, setSelectedId } = useContext(Contexto);
+
+
+const { selectedId, 
+    setSelectedId,
+    setCandidato1,
+    candidato1,
+    setCandidato2,
+    candidato2,
+    setCandidato4,
+    candidato4,
+    candidato3,
+    setCandidato3,
+    setCandidato5,
+    candidato5,
+    setCandidato6,
+    candidato6,
+    setCandidato7,
+    candidato7,
+    setCandidato8,
+    candidato8
+} = useContext(Contexto);
+
+
 
 const votoCandidato=()=>{
     if(props.personero){
-        switch(selectedId){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
+        Candidatos.personeros.map((elejido)=>{
+            if(elejido.id==selectedId){
+                switch(selectedId){
+                    case 1:
+                        setCandidato1(candidato1+1)
+                        {/*Alert.alert('votos del candidato 1: '+ candidato1)*/}
+                        break;
+                    case 2:
+                        setCandidato2(candidato2+1)
+                        break;
+                    case 3:
+                        setCandidato3(candidato3+1)
+                        break;
+                    case 4:
+                        setCandidato4(candidato4+1)
+                        break;
+                }
+            }
+        })
     }if(props.contralor){
         switch(selectedId){
             case 5:
-                
+                setCandidato5(candidato5+1)
                 break;
             case 6:
+                setCandidato6(candidato6+1)
                 break;
             case 7:
+                setCandidato7(candidato7+1)
                 break;
             case 8:
+                setCandidato8(candidato8+1)
                 break;
         }
     }
 }
 
+
 const goToPage = () => {
-    if (props.contralor) {
-    Candidatos.contralor.map((e) => {
-        if (e.id == selectedId) {
-        Alert.alert("Ha votado por: " + e.Nombre);
-        }
-    });
+    if (props.contralor){
     setSelectedId(null);
     props.navegacion.navigate(Personero);
     }
-    if (props.personero) {
-    Candidatos.personeros.map((e) => {
-        if (e.id == selectedId) {
-        Alert.alert("Ha votado por: " + e.Nombre);
-        }
-    });
+    if (props.personero){
     setSelectedId(null);
     props.navegacion.navigate(Contralor);
     }
@@ -74,6 +99,7 @@ const handleVoto = () => {
             Alert.alert("Ha votado por: " + e.Nombre);
         }
         });
+        votoCandidato();
         setSelectedId(null);
         goToPage();
     }
--- context/Contexto.js
+++ context/Contexto.js
@@ -4,41 +4,40 @@ import { createContext,useState, useEffect } from "react";
 export const Contexto = createContext();
 
 export const MyContexto=(props)=>{
-    const [candidato1, setcandidato1] = useState(0)
-    const [candidato2, setcandidato2] = useState(0)
-    const [candidato3, setcandidato3] = useState(0)
-    const [candidato4, setcandidato4] = useState(0)
-    const [candidato5, setcandidato5] = useState(0)
-    const [candidato6, setcandidato6] = useState(0)
-    const [candidato7, setcandidato7] = useState(0)
-    const [candidato8, setcandidato8] = useState(0)
-    const [candidato9, setcandidato9] = useState(0)
-    const [votoBlancoPersonero, setVotoBlancoPersonero] = useState(0)
-    const [votoBlancoContralor, setVotoBlancoContralor] = useState(0)
+    const [candidato1, setCandidato1] = useState(0);
+    const [candidato2, setCandidato2] = useState(0);
+    const [candidato3, setCandidato3] = useState(0);
+    const [candidato4, setCandidato4] = useState(0);
+    const [candidato5, setCandidato5] = useState(0);
+    const [candidato6, setCandidato6] = useState(0);
+    const [candidato7, setCandidato7] = useState(0);
+    const [candidato8, setCandidato8] = useState(0);
 
-    const [mostrar,setMostrar]=useState(true);
+    const [mostrar,setMostrar]=useState(false);
 
     const [selectedId, setSelectedId] = useState(null);
 
-    const [password, setPassword] = useState('');
-
-    const candidatos={
-        setcandidato1,
-        setcandidato2,
-        setcandidato3,
-        setcandidato4,
-        setcandidato5,
-        setcandidato6,
-        setcandidato7,
-        setcandidato8,
-        setcandidato9,
-        setVotoBlancoContralor,
-        setVotoBlancoPersonero
-    }
-    
+    const [password, setPassword] = useState('')
 
     return(
-        <Contexto.Provider value={{selectedId,setSelectedId,candidatos,mostrar,setMostrar, setPassword, password}}>
+        <Contexto.Provider value={{selectedId,setSelectedId,mostrar,setMostrar, setPassword, password
+        , setCandidato1,
+        setCandidato2,
+        setCandidato3,
+        setCandidato5,
+        setCandidato6,
+        setCandidato7,
+        setCandidato4,
+        setCandidato8, 
+        candidato1,
+        candidato2,
+        candidato3,
+        candidato4,
+        candidato5,
+        candidato6,
+        candidato7,
+        candidato8
+        }}>
             {props.children}
         </Contexto.Provider>
     );
